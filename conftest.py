ccf37faa04fc2c948f44ff15711186ce2ee8b06c
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-01-30 18:45:39 -0700

    add hypothesis profile

--- conftest.py
+++ conftest.py
@@ -1,5 +1,10 @@
 import logging
 
+from hypothesis import settings
 
 # turn off Numba logging
 logging.getLogger('numba').setLevel(logging.INFO)
+
+
+# set up profiles
+settings.register_profile('large', max_examples=5000)
