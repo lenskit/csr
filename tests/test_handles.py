6da35117694db0f06b8a344e195c7f46f6549605
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-02-04 16:16:12 -0700

    add basic handle test

---
+++ tests/test_handles.py
@@ -0,0 +1,23 @@
+"""
+Tests for creating kernel handles.
+"""
+
+from csr import CSR
+from csr.test_utils import csrs, sparse_matrices
+
+from hypothesis import given, settings
+
+
+@settings(deadline=500)
+@given(sparse_matrices())
+def test_make_handle(kernel, mat):
+    csr = CSR.from_scipy(mat)
+    h = kernel.to_handle(csr.R)
+    try:
+        assert h is not None
+        c2 = kernel.from_handle(h)
+        assert c2.nrows == csr.nrows
+        assert c2.ncols == csr.ncols
+        assert c2.nnz == csr.nnz
+    finally:
+        kernel.release_handle(h)
