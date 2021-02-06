b8d51dda16bf899341557d3ca3eb20b2d990b2ca
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-02-05 20:04:27 -0700

    export order_column api

--- csr/kernel.py
+++ csr/kernel.py
@@ -9,6 +9,7 @@ _kernel = _default_kernel()
 to_handle = _kernel.to_handle
 from_handle = _kernel.from_handle
 release_handle = _kernel.release_handle
+order_columns = _kernel.order_columns
 mult_ab = _kernel.mult_ab
 mult_abt = _kernel.mult_abt
 mult_vec = _kernel.mult_vec
