ffd2d4247d5e6ed393bd993fa2fa167522bede60
Michael Ekstrand <md@ekstrandom.net>
2021-06-26 16:42:22 -0600

    add large multiplication

--- tests/test_mkl.py
+++ tests/test_mkl.py
@@ -6,6 +6,7 @@ import pytest
 from numba import njit
 import numpy as np
 
+from csr import CSR
 from csr.test_utils import csrs
 
 from hypothesis import given
@@ -40,3 +41,39 @@ def test_csr_handle(csr):
         assert np.all(csr2.values == csr.values)
     else:
         assert np.all(csr2.values == 1.0)
+
+
+def test_large_mult_vec():
+    # 10M * 500 = 2.5B >= INT_MAX
+    nrows = 10000000
+    ncols = 500
+    dense = 250
+    nnz = nrows * dense
+
+    rowptrs = np.arange(0, nnz + 1, dense, dtype=np.int64)
+
+    assert len(rowptrs) == nrows + 1
+    assert rowptrs[-1] == nnz
+
+    try:
+        print('allocating indexes')
+        colinds = np.empty(nnz, dtype=np.intc)
+        print('allocating values')
+        values = np.random.randn(nnz)
+    except MemoryError:
+        pytest.skip('insufficient memory')
+
+    print('randomizing colinds')
+    for i in range(nrows):
+        s = i * dense
+        e = s + dense
+        colinds[s:e] = np.random.choice(ncols, dense, replace=False)
+
+    csr = CSR(nrows, ncols, nnz, rowptrs, colinds, values)
+
+    v = np.random.nrand(ncols)
+
+    res = csr.mult_vec(v)
+
+    assert res.shape == (nrows,)
+    assert np.all(~np.isnan(res))
