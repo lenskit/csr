01d3780996d97ceebac7fea9261fd6b2a72415bb
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-06-29 14:23:17 -0600

    a few more samples

--- conftest.py
+++ conftest.py
@@ -41,7 +41,7 @@ def kernel(request):
 settings.register_profile('default', deadline=1500)
 settings.register_profile('large', settings.get_profile('default'),
                           max_examples=5000, deadline=None)
-settings.register_profile('fast', max_examples=10)
+settings.register_profile('fast', max_examples=20)
 settings.register_profile('nojit', settings.get_profile('fast'),
                           deadline=None)
 settings.load_profile('default')
