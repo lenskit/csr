616f96a248aeaac9a17377177ad1f0a1eac12c2f
Michael Ekstrand <michaelekstrand@boisestate.edu>
2023-04-18 16:44:10 -0600

    clear up MKL flakes

--- csr/kernels/mkl/__init__.py
+++ csr/kernels/mkl/__init__.py
@@ -1,5 +1,5 @@
 import numpy as np
-from .handle import to_handle, from_handle, release_handle, order_columns
-from .multiply import mult_ab, mult_abt, mult_vec
+from .handle import to_handle, from_handle, release_handle, order_columns  # noqa: F401
+from .multiply import mult_ab, mult_abt, mult_vec  # noqa: F401
 
 max_nnz = np.iinfo('i4').max
