a75144dca8920ad2820f111d93d2db3ff21dfd7c
Michael Ekstrand <md@ekstrandom.net>
2024-06-10 16:45:45 -0400

    tweak shard size

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -172,7 +172,7 @@ def test_filter(csr):
 @csr_slow()
 @given(csrs(st.integers(10, 100), st.integers(10, 100), max_density=0.5, values=True))
 def test_shard(csr):
-    SHARD_SIZE = 10
+    SHARD_SIZE = 500
 
     shards = csr._shard_rows(SHARD_SIZE)
     # we have the whole matrix
